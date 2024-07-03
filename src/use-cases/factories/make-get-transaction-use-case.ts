e5cde3f99541851a7e88aea0402b7e11f9ca236e
Lucas Pevidor <lucas.pevidor@gmail.com>
2024-07-03 20:34:07 -0300

    Added types to repository variables

--- src/use-cases/factories/make-get-transaction-use-case.ts
+++ src/use-cases/factories/make-get-transaction-use-case.ts
@@ -1,10 +1,13 @@
 import { PrismaTransactionRepository } from "@/repositories/prisma/prisma-transaction-repository";
 import { PrismaUsersRepository } from "@/repositories/prisma/prisma-users-repository";
+import { TransactionRepository } from "@/repositories/transaction-repository";
+import { UsersRepository } from "@/repositories/user-repository";
 import { GetTransactionUseCase } from "../get-transaction";
 
 export async function MakeGetTransactionUseCase() {
-  const usersRepository = new PrismaUsersRepository();
-  const transactionRepository = new PrismaTransactionRepository();
+  const usersRepository: UsersRepository = new PrismaUsersRepository();
+  const transactionRepository: TransactionRepository =
+    new PrismaTransactionRepository();
 
   return new GetTransactionUseCase(usersRepository, transactionRepository);
 }
