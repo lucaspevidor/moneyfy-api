32b9180698aec6bc03e1b37ac530b8889ab26335
Lucas Pevidor <lucas.pevidor@gmail.com>
2024-07-10 14:37:53 -0300

    Implemented refresh-token controller

--- src/http/controllers/users/routes.ts
+++ src/http/controllers/users/routes.ts
@@ -3,12 +3,14 @@ import { FastifyInstance } from "fastify";
 import { AuthenticateUser } from "./authenticate-user";
 import { DeleteUser } from "./delete-user";
 import { GetUserProfile } from "./get-user-profile";
+import { RefreshToken } from "./refresh-token";
 import { RegisterUser } from "./register-user";
 import { UpdateUser } from "./update-user";
 
 export async function usersRoutes(app: FastifyInstance) {
   app.post("/users", RegisterUser);
   app.post("/sessions", AuthenticateUser);
+  app.patch("/token/refresh", RefreshToken);
 
   app.get("/profile", { onRequest: [jwtVerify] }, GetUserProfile);
   app.patch("/users", { onRequest: [jwtVerify] }, UpdateUser);
